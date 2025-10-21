30103f5664435b8737bee4ee79258849ea34109b
SangWon7242 <gotkdqja@gmail.com>
2025-10-21 21:39:09 +0900

    세팅

---
+++ src/main/java/com/back/FlyIoPgApp251021Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyIoPgApp251021Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlyIoPgApp251021Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/back/FlyIoPgApp251021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyIoPgApp251021ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
